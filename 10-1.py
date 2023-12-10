4c46f732966a73e484410176d52b3425393a8f6f
Lars <lars.okken@gmail.com>
2023-12-10 07:04:27 +0000

    Day 10

---
+++ 10-1.py
@@ -0,0 +1,124 @@
+f = open("10.txt").readlines()
+
+
+
+
+for i,line in enumerate(f):
+    if "S" in line:
+        start = (i,line.index("S"))
+
+def surrounding(i,j):
+    out = []
+    c = f[i][j]
+    
+    if c == "|":
+        if i - 1 >= 0:
+            out.append((i-1,j))
+        if i + 1 < len(f):
+            out.append((i+1, j))
+    elif c == "-":
+        if j - 1 >= 0:
+            out.append((i,j-1))
+        if j + 1 < len(f[i]):
+            out.append((i, j+1))
+    elif c == "L":
+        if i - 1 >= 0:
+            out.append((i-1,j))
+        if j + 1 < len(f[i]):
+            out.append((i, j+1))
+    elif c == "J":
+        if i - 1 >= 0:
+            out.append((i-1,j))
+        if j - 1 >= 0:
+            out.append((i, j-1))
+    elif c == "7":
+        if i + 1 < len(f):
+            out.append((i+1,j))
+        if j - 1 >= 0:
+            out.append((i, j-1))
+    elif c == "F":
+        if i + 1 < len(f):
+            out.append((i+1,j))
+        if j + 1 < len(f[i]):
+            out.append((i, j+1))
+    elif c == "S":
+
+        for a in range(-1,2):
+            if a + i < 0 or a + i >= len(f):
+                continue
+            out.append((i+a,j))
+
+        for b in range(-1,2):
+            if b + j < 0 or b + j >= len(f[i]):
+                continue
+            out.append((i,j+b))
+        out.remove((i,j))
+    return out
+
+distances = [[-1 for i in line] for line in f]
+
+def traverse(i,j, visited = []):
+    visited.append((i,j))
+    finished = False
+    currentLayer = [(i,j)]
+    layer = 1
+    while not finished:
+        newLayer = []
+        for pos in currentLayer:
+            i,j = pos
+            for newPos in surrounding(i,j):
+                
+                a,b = newPos
+                c = f[a][b]
+                if (a,b) in visited:
+                    continue
+                if c == "|":
+                    if a!=i and b==j:
+                        visited.append((a,b))
+                        newLayer.append((a,b))
+                        if distances[a][b] == -1:
+                            distances[a][b] = layer
+
+                elif c == "-":
+                    if b != j and a==i:
+                        visited.append((a,b))
+                        newLayer.append((a,b))
+                        if distances[a][b] == -1:
+                            distances[a][b] = layer
+                elif c == "L":
+                    if (a == i+1 and b==j) or (a==i and b == j-1):
+                        visited.append((a,b))
+                        newLayer.append((a,b))
+                        if distances[a][b] == -1:
+                            distances[a][b] = layer
+                elif c == "J":
+                    if (a == i+1 and b==j) or (a==i and b == j+1):
+                        visited.append((a,b))
+                        newLayer.append((a,b))
+                        if distances[a][b] == -1:
+                            distances[a][b] = layer
+                elif c == "7":
+                    if (a == i-1 and b==j) or (a==i and b == j+1):
+                        visited.append((a,b))
+                        newLayer.append((a,b))
+                        if distances[a][b] == -1:
+                            distances[a][b] = layer
+                elif c == "F":
+                    if (a == i-1 and b==j) or (a==i and b == j-1):
+                        visited.append((a,b))
+                        newLayer.append((a,b))
+                        if distances[a][b] == -1:
+                            distances[a][b] = layer
+                elif c == "S":
+                    visited.append((a,b))
+                    newLayer.append((a,b))
+                    if distances[a][b] == -1:
+                            distances[a][b] = layer
+                max(1,2)
+        layer += 1
+        currentLayer = newLayer[:]
+        if len(currentLayer) == 0:
+            return max(max(i) for i in distances)
+
+
+print(traverse(start[0], start[1]))
